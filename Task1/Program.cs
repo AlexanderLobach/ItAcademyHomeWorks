c23889e15191ac48923a38dc5998c5cb94634071
AlexanderLobach <375296255246@ya.ru>
2019-09-27 23:48:33 +0300

    add implicit type conversion

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -8,7 +8,7 @@ namespace Task1
         {
             // explicit type conversion
              Console.ForegroundColor = ConsoleColor.Blue;
-            Console.WriteLine("       explicit type conversion \n");
+            Console.WriteLine("\n\n       explicit type conversion \n");
             Console.ForegroundColor = ConsoleColor.White;
             Console.WriteLine(" enter a number between -128 and 127 (sbyte type)");
             string num= Console.ReadLine();
@@ -17,11 +17,20 @@ namespace Task1
             Int16 num2;
             num2 = Convert.ToInt16(num1);
             Int32 num3;
-            num3 = Convert.ToInt32 (num1);
+            num3 = Convert.ToInt32 (num2);
             float num4;
-            num4 = Convert.ToSingle(num1);
+            num4 = Convert.ToSingle(num3);
             Console.ForegroundColor = ConsoleColor.Green;
-            Console.WriteLine(string.Format("Int16={0} Int32={1} float={2}", num2,  num3, num4 ));
+            Console.WriteLine(string.Format("sbyte to int16= {0}   int16 to int32= {1}   int 32 to float= {2}", num2,  num3, num4 ));
+
+                    //implicit type conversion
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("\n       implicit type conversion \n");
+            Console.ForegroundColor = ConsoleColor.Green;
+            float num5= num1;
+            double num6= num2;
+            decimal num7= num3;
+            Console.WriteLine(string.Format("sbyte to float= {0}   int16 to doble= {1}   int32 to decimal= {2}", num5,  num6, num7 ));
             Console.ReadKey();
         }
     }
