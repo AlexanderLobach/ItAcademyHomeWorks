d545d35e896fc220834a1e7db6e55b24dfad9f15
AlexanderLobach <375296255246@ya.ru>
2019-10-04 23:03:17 +0300

    HW_05_Task1

---
+++ HW_05_Task1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace HW_05_Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             Console.WriteLine("Hello gyus!");
+            int i;
+            int [] mass1 = new int[5];
+            Random rand = new Random();
+            for (i = 0; i < mass1.Length; i++)
+            {
+                mass1[i] = rand.Next(0, 100);
+                Console.WriteLine($"mass1[{i}] = {mass1[i]}");
+            }  
+            int [] mass2 = new int[5];
+            for (i = 0; i < mass2.Length; i++)
+            {
+
+                Console.WriteLine($"Enter value {i} for array mass2");
+                mass2[i] =Convert.ToInt32(Console.ReadLine());
+            }
+
+            int [] mass3 = new int[5];
+            for (i = 0; i < mass3.Length; i++)
+            {
+
+                mass3[i] = mass1[i] + mass2[i];
+                Console.WriteLine($"mass1[{i}] = {mass1[i]}      mass2[{i}] = {mass2[i]}       mass3[{i}] = {mass3[i]}"); 
+            }
+
+
+        }
+    }
+}
