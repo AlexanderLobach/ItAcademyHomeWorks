bca25d505c183890624f0c469dedd973d371a8ba
AlexanderLobach <375296255246@ya.ru>
2019-10-24 12:49:36 +0300

    Add AssemblyTwo

---
+++ AssemblyTwo/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using AssemblyOne;
+
+
+
+namespace AssemblyTwo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Motorcycle koljaska = new Motorcycle();
+            //koljaska.typeVehicleInternal = "motokoljaska"; - is inaccessible due to its protection level
+            //koljaska.maxSpeedPrivate = 400; - is inaccessible due to its protection level
+            //koljaska.vinCodeProtected = "000 000000qwefqe 0a00afa"; - is inaccessible due to its protection level
+            //koljaska.adometerProtectedInternal = 0001; - is inaccessible due to its protection level
+            //koljaska.motoHoursPrivateProtected = 0934850; - is inaccessible due to its protection level
+            koljaska.StartEnginePublic();
+            //BikeSport kawasaki = new BikeSport(); - is inaccessible due to its protection level
+        }
+    }
+}
