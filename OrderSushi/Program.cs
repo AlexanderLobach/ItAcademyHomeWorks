3e158fe52df7a5d794a6021599b4d7e753a8a534
AlexanderLobach <375296255246@ya.ru>
2019-11-25 09:27:48 +0300

    Aadded a project OrderSushi for the VStudio

---
+++ OrderSushi/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace OrderSushi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Messager messager = new Messager();
+            messager.Welcome();
+            messager.ReadName();
+            
+            //messager.OrderRequest();
+            //Console.WriteLine(messager.FindingMatches("maki", messager.SushiList));
+            Console.WriteLine(messager.OrderRequest());
+        }
+    }
+}
---
+++ OrderSushi/Sushi.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace OrderSushi
+{
+    public class Sushi
+    {
+        public string filodelfia = "filodelfia";
+        public string gomosjakiMaki = "gomosjaki maki";
+        public string[] SushiList = new string [] {"filodelfia kaki", "gomosjaki maki", "carpi"};
+    }
+}
