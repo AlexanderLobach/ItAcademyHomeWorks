3974a21923bb3e3ddd5874e2b300609a7cb23f97
AlexanderLobach <375296255246@ya.ru>
2019-12-01 02:37:04 +0300

    Added class Searcher Modified class GetSetSqlData Modified class Messager Modified class Sushi

--- OrderSushiMonodev/OrderSushi/OrderSushi/Sushi.cs
+++ OrderSushiMonodev/OrderSushi/OrderSushi/Sushi.cs
@@ -4,15 +4,25 @@ namespace OrderSushi
 {
 	public class Sushi : GetSetSqlData
 	{
-		public string filodelfia = "filodelfia";
-		public string gomosjakiMaki = "gomosjaki maki";
-		public string SushiList { get; set;}
-	
-	public void GetSetSushilist()
-	{
+		public string SushiName { get; set; }
+		public string SushiInfo { get; set; }
+		public string SushiList { get; set; }
+
+		public void GetSetSushilist()
+		{
+			this.sqlRequest = $"SELECT sushi_name FROM {nameSqlSushiList} WHERE sushi_id is not NULL";
 			GetSqlArray ();
-			this.SushiList = this.dataArrString;
-	}
+			this.SushiList = dataArrString;
+		}
+
+		public void GetSushiInfo()
+		{
+			this.sqlRequest = $"SELECT  sushi_price, sushi_weight, sushi_description FROM {nameSqlSushiList} WHERE sushi_name = '{SushiName}'";
+			GetSqlArray ();
+			this.SushiInfo = dataArrString;
+
+		}
+
 
 	}
 }
